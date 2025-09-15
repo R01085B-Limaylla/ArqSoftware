42af08a4f66290491dd7e220257565fdda2c7cf0
R01085B-Limaylla <r01085b@ms.upla.edu.pe>
2025-09-15 00:47:09 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // Cambia a '/TU-REPO/' si publicas en subcarpeta de GitHub Pages
-  base: './'
+  base: '/ArqSoftware/'
 })
